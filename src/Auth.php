b941cfb742e0016faa7991eea97646b4e4d67b6d
samuel.rockenbach <samuel.rockenbach@universo.univates.br>
2024-09-29 22:43:18 -0300

    Add key auth

---
+++ src/Auth.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Rosa\Router;
+
+class Auth
+{
+    /**
+     * Check if API-Key is valid
+     * 
+     * * The method used is the HTTP_KEY Header
+     * @example On the REST-client just inform the param KEY: <your_key> on the Header
+     * 
+     * @param string $api_key
+     * @param string $client_key
+     * @return void
+     */
+    public static function check($api_key, $client_key)
+    {
+        if (!$api_key)
+            Response::json([
+                'error' => 'API-Key could not be loaded'
+            ], 403);
+
+        if (!$client_key)
+            Response::json([
+                'error' => 'API-Key was not provided'
+            ], 403);
+
+        if (!hash_equals($api_key, $client_key))
+            Response::json([
+                'error' => 'Incorrect API-Key provided'
+            ], 403);
+    }
+}
