156f7f5271a2cb044c1bf8ff907271ed237c020b
samuel.rockenbach <samuel.rockenbach@universo.univates.br>
2024-10-03 23:01:33 -0300

    Several examples

--- routes/routes.php
+++ routes/routes.php
@@ -6,6 +6,17 @@ use Rosa\Controllers\UserController;
 use Rosa\Controllers\V1\ExampleController as V1ExampleController;
 use Rosa\Controllers\V2\ExampleController as V2ExampleController;
 
+/**
+ * Simple routes
+ */
+Route::post('/user/', [
+    UserController::class, 'post'
+])->private();
+
+Route::get('/post/{post}/comment/{comment}', [
+    PostController::class, 'get'
+])->public();
+
 Route::group('v1', function() {
     Route::get('/example/{id}', [
         V1ExampleController::class, 'get'
@@ -16,6 +27,9 @@ Route::group('v1', function() {
     ])->private();
 });
 
+/**
+ * Grouped routes
+ */
 Route::group('v2', function() {
     Route::get('/example/{id}', [
         V2ExampleController::class, 'get'
@@ -26,10 +40,37 @@ Route::group('v2', function() {
     ])->private();
 });
 
-Route::post('/user/', [
-    UserController::class, 'post'
-])->private();
+/**
+ * Nested groups
+ */
+Route::group('multilevel', function() {
+    Route::group('1', function() {
+        Route::get('/example/{id}', [
+            V2ExampleController::class, 'get'
+        ])->private();
 
-Route::get('/post/{post}/comment/{comment}', [
-    PostController::class, 'get'
-])->public();
+        Route::post('/example/', [
+            V2ExampleController::class, 'post'
+        ])->private();
+
+        Route::group('2', function() {
+            Route::get('/example/{id}', [
+                V2ExampleController::class, 'get'
+            ])->private();
+        
+            Route::post('/example/', [
+                V2ExampleController::class, 'post'
+            ])->private();
+        });
+
+        Route::group('3', function() {
+            Route::get('/example/{id}', [
+                V2ExampleController::class, 'get'
+            ])->private();
+        
+            Route::post('/example/', [
+                V2ExampleController::class, 'post'
+            ])->private();
+        });
+    });
+});
