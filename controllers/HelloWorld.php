ac556cbd62febcc7ee2c2fd834e26403764833b5
samuel.rockenbach <samuel.rockenbach@universo.univates.br>
2024-12-07 23:48:22 -0300

    Fix imports

--- controllers/HelloWorld.php
+++ controllers/HelloWorld.php
@@ -4,6 +4,12 @@ namespace Rockberpro\RestRouter\Controllers;
 
 use Rockberpro\RestRouter\Response;
 
+/**
+ * @author Samuel Oberger Rockenbach
+ * 
+ * @version 1.0
+ * @package Rockberpro\RestRouter
+ */
 class HelloWorld
 {
     public function hello()
