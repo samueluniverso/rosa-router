9b35977f044a89dbfbece06505267c02a7916cb4
samuel.rockenbach <samuel.rockenbach@universo.univates.br>
2024-12-14 16:32:50 -0300

    fixup! Add prefix to route-mapping

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,6 @@
 use Rockberpro\RestRouter\Route;
 use Rockberpro\RestRouter\Controllers\AuthController;
 use Rockberpro\RestRouter\Controllers\HelloWorld;
-use Rockberpro\RestRouter\Response;
-
-Route::get('/test/{id}', function() {
-    Response::json([
-        'message' => 'Apenas um teste'
-    ], 200);
-})->public();
 
 Route::prefix('auth')->group(function() {
     Route::get('/refresh', [
