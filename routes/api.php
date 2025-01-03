2b7d422b4ba3405f7233c0230cee89c9877671e6
samuel.rockenbach <samuel.rockenbach@universo.univates.br>
2025-01-03 00:58:56 -0300

    Remove HelloWorld Controller

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,13 @@
 
 use Rockberpro\RestRouter\Route;
 use Rockberpro\RestRouter\Controllers\AuthController;
-use Rockberpro\RestRouter\Controllers\HelloWorldController;
 
 Route::prefix('auth')->group(function() {
     Route::post('/refresh', [
         AuthController::class, 'refresh'
-    ])->public();
+    ]);
 
     Route::post('/access', [
         AuthController::class, 'access'
-    ])->public();
+    ]);
 });
-
-Route::get('/hello', [
-    HelloWorldController::class, 'hello'
-])->private();
